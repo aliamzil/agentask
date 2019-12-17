a4aecb95a4d145d82457db35f123b556ba0371e9
Paul Aubry <thekom.7835@gmail.com>
2019-12-17 16:41:22 +0100

    metodes mail : confirmation inscription ; mot de passe perdu - paul - dev

--- src/main/java/fr/dawan/agentask/controllers/testcontrol.java
+++ src/main/java/fr/dawan/agentask/controllers/testcontrol.java
@@ -2,10 +2,15 @@ package fr.dawan.agentask.controllers;
 
 import java.time.LocalDate;
 
+import javax.mail.Message;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -19,10 +24,12 @@ public class testcontrol {
 	
 	@Autowired
 	UserDao ud;
-	
 	@Autowired
 	ServiceUser su;
-	
+    @Autowired
+    private JavaMailSender emailSender;
+    @Autowired
+    CryptitudeDao cd;
 	
 	@GetMapping("/testuser404")
 	public String testuser(HttpServletRequest request) {
@@ -30,15 +37,29 @@ public class testcontrol {
 		String res = "home";
 		
 		User u1 = null;
-		String co = su.connect(u1,"Test", "pass123456", session);
+		String co = su.connect(u1,"Roger2", "rogerpass", session);
 		if(session.getAttribute("user")!=null) {
 			u1 = (User) session.getAttribute("user");
 			System.out.println(u1.getPseudo());
 		}
 		
-//		su.sendmailuser("thekom.7835@gmail.com", "test mail", "Mail de test pour java");
+		su.passperdu(u1);
+		
+		su.createuser("Roberto", "rbt@mail.com", "blabla", "blabla", LocalDate.now());
 		
 		return res;
+	}
+
+	@GetMapping("/testmailhtml")
+	public String testMailhtml() {
+		su.envoimail("test@mail.com", "essai@mail.com", "Sujet du mail", "Texte du mail");
+		return "home";
+	}
+
+	@GetMapping("/crypt")
+	public String crypter() {
+		System.out.println(cd.cryptage("rogerpass"));
+		return "home";
 	}
 
 }
